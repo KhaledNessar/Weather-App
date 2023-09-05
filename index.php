a60cd98a419b59fd39286837384c4a9b95039ece
KhaledNessar <khaled.nessar@wambo.com>
2023-09-05 13:48:15 +0200

    drittes Update

--- index.php
+++ index.php
@@ -1,92 +1,110 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="css/styles.css">
-        <link rel="icon" href="http://openweathermap.org/img/wn/10d@2x.png"/>
-        <link
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/styles.css">
+    <link
             rel="stylesheet"
             href="//fonts.googleapis.com/css?family=Just+Another+Hand"
-        />
-        <title>Weather App</title>
-    </head>
-    <body>
+    />
+    <title>Weather App</title>
+</head>
+<body>
 
 
-        <h1 class="title">Weather Forecast For Today</h1>
+<h1 class="title">Weather Forecast For Today</h1>
 
-        <div class="box">
-            <form method="post" action="index.php" class="form">
-                Weather in:
-                <input type="text" name="cityName" placeholder="Please enter a city name!">
-                <input type="submit" value="Search" name="submit">
-            </form>
-        </div>
+<div class="box">
+    <form method="post" action="index.php" class="form">
+        <label for="cityName">Weather in:</label>
+        <input type="text" name="cityName" placeholder="Please enter a city name!" required>
+        <input type="submit" value="Search" name="submit">
+    </form>
+</div>
 
-        <?php
-        if (isset($_POST["submit"])) {
-            $city = $_POST["cityName"];
-            $apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=$city&units=metric&appid=abb21b96237441179709d635f51a8f80";
+<?php
 
-            $json_data = file_get_contents($apiUrl);
-            $city_data = json_decode($json_data);
+include("config.php");
 
-            // Check if the 'city' key exists in the response data to avoid errors
-            if (isset($city_data->city)) {
-                $city_list = count($city_data->list);
+if (isset($_POST["submit"])) {
+    $city = $_POST["cityName"];
+    $apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=$city&units=metric&appid=$apiKey";
 
-                echo '<div class="city_title">';
-                echo '<h1>', $city_data->city->name, '(', $city_data->city->country,')</h1>';
+    try {
+        $json_data = file_get_contents($apiUrl);
+        if ($json_data === false) {
+            throw new Exception('Failed to retrieve data from the API.');
+        }
+
+        $city_data = json_decode($json_data);
+
+        if ($city_data === null || !isset($city_data->city)) {
+            throw new Exception("City not found");
+        }
+    }
+    catch (Exception $e) {
+        echo 'Message: ' . $e->getMessage();
+    }
+
+
+    $city_data = json_decode($json_data);
+
+    // Check if the 'city' key exists in the response data to avoid errors
+    if (isset($city_data->city)) {
+        $city_list = count($city_data->list);
+
+        echo '<div class="city_title">';
+        echo '<h1>', $city_data->city->name, '(', $city_data->city->country,')</h1>';
+        echo '</div>';
+
+        echo '<div class="container">';
+
+        $currentTimestamp = time(); // Aktuelle Serverzeit in Unix-Timestamp erhalten
+        $today = date("d/m/y", $currentTimestamp); // Heutiges Datum
+
+        for ($i = 0, $count = 0; $i < $city_list && $count < 5 ; $i++) {
+            $timestamp = strtotime($city_data->list[$i]->dt_txt);
+            $forecastDate = date("d/m/y", $timestamp);
+            $formattedTime = date("H:i", $timestamp);
+
+            if ($timestamp >= $currentTimestamp) {
+                echo '<div class="item">';
+                echo '<h1>', $forecastDate, ' ', $formattedTime, '</h1>';
+                $iconName = $city_data->list[$i]->weather[0]->icon;
+                $iconLink = "https://openweathermap.org/img/wn/" . $iconName . "@2x.png";
+
+                echo "<img src='$iconLink'>";
+
+                // General information about the weather
+                echo '<h2>Temperature</h2>';
+                echo '<p><strong>Current: </strong>', $city_data->list[$i]->main->temp, '&deg; C</p>';
+                echo '<p><strong>Minimum: </strong>', $city_data->list[$i]->main->temp_min, '&deg; C</p>';
+                echo '<p><strong>Maximum: </strong>', $city_data->list[$i]->main->temp_max, '&deg; C </p>';
+
+                // Something about the air
+                echo '<h2>Air</h2>';
+                echo '<p><strong>Humidity: </strong>', $city_data->list[$i]->main->humidity, '%</p>';
+                echo '<p><strong>Pressure: </strong>', $city_data->list[$i]->main->pressure, 'hpa</p>';
+
+                // Some info about the wind
+                echo '<h2>Wind</h2>';
+                echo '<p><strong>Speed: </strong>', $city_data->list[$i]->wind->speed, 'm/s</p>';
+                echo '<p><strong>Orientation: </strong>', $city_data->list[$i]->wind->deg, '&deg; </p>';
+
+                // Description
+                echo '<h2>The weather</h2>';
+                echo '<p><strong>Description: </strong>', $city_data->list[$i]->weather[0]->description, '</p>';
                 echo '</div>';
-
-                echo '<div class="container">';
-
-                $currentTimestamp = time(); // Aktuelle Serverzeit in Unix-Timestamp erhalten
-                $today = date("d/m/y", $currentTimestamp); // Heutiges Datum
-
-                for ($i = 0, $count = 0; $i < $city_list && $count < 5 ; $i++) {
-                    $timestamp = strtotime($city_data->list[$i]->dt_txt);
-                    $forecastDate = date("d/m/y", $timestamp);
-                    $formattedTime = date("H:i", $timestamp);
-
-                    if ($timestamp >= $currentTimestamp) {
-                        echo '<div class="item">';
-                        echo '<h1>', $forecastDate, ' ', $formattedTime, '</h1>';
-                        $iconName = $city_data->list[$i]->weather[0]->icon;
-                        $iconLink = "http://openweathermap.org/img/wn/" . $iconName . "@2x.png";
-
-                        echo "<img src='$iconLink'>";
-
-                        // General information about the weather
-                        echo '<h2>Temperature</h2>';
-                        echo '<p><strong>Current: </strong>', $city_data->list[$i]->main->temp, '&deg; C</p>';
-                        echo '<p><strong>Minimum: </strong>', $city_data->list[$i]->main->temp_min, '&deg; C</p>';
-                        echo '<p><strong>Maximum: </strong>', $city_data->list[$i]->main->temp_max, '&deg; C </p>';
-
-                        // Something about the air
-                        echo '<h2>Air</h2>';
-                        echo '<p><strong>Humidity: </strong>', $city_data->list[$i]->main->humidity, '%</p>';
-                        echo '<p><strong>Pressure: </strong>', $city_data->list[$i]->main->pressure, 'hpa</p>';
-
-                        // Some info about the wind
-                        echo '<h2>Wind</h2>';
-                        echo '<p><strong>Speed: </strong>', $city_data->list[$i]->wind->speed, 'm/s</p>';
-                        echo '<p><strong>Orientation: </strong>', $city_data->list[$i]->wind->deg, '&deg; </p>';
-
-                        // Description
-                        echo '<h2>The weather</h2>';
-                        echo '<p><strong>Description: </strong>', $city_data->list[$i]->weather[0]->description, '</p>';
-                        echo '</div>';
-                        $count++;
-                    }
-                }
-                echo '</div>';
-            } else {
-                echo '<div class="error">City not found</div>';
+                $count++;
             }
         }
-        ?>
+        echo '</div>';
+    } else {
+        echo '<div class="error">City not found</div>';
+    }
+}
+?>
 
-    </body>
+</body>
 </html>
