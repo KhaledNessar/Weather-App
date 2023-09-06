de84a1d030b501d17d5742b4eed0f901624a6228
KhaledNessar <khaled.nessar@wambo.com>
2023-09-06 13:15:37 +0200

    Sechstes Update

--- index.php
+++ index.php
@@ -34,79 +34,88 @@ if (isset($_POST["submit"])) {
     $city = $_POST["cityName"];
     $apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=$city&units=metric&appid=$apiKey";
 
-    try {
-        $json_data = file_get_contents($apiUrl);
-        if ($json_data === false) {
-            throw new Exception('Failed to retrieve data from the API.');
-        }
+// Use the "get_headers" function to check the HTTP response headers
+    $response_headers = get_headers($apiUrl);
 
-        $city_data = json_decode($json_data);
-
-        if ($city_data === null || !isset($city_data->city)) {
-            throw new Exception("City not found");
-        }
-    }
-    catch (Exception $e) {
-        echo 'Message: ' . $e->getMessage();
-    }
-
-
-    $city_data = json_decode($json_data);
-
-    // Check if the 'city' key exists in the response data to avoid errors
-    if (isset($city_data->city)) {
-        $city_list = count($city_data->list);
-
-        echo '<div class="city_title">';
-        echo '<h1>', $city_data->city->name, '(', $city_data->city->country,')</h1>';
-        echo '</div>';
-
-        echo '<div class="container">';
-
-        $currentTimestamp = time(); // Aktuelle Serverzeit in Unix-Timestamp erhalten
-        $today = date("d/m/y", $currentTimestamp); // Heutiges Datum
-
-        for ($i = 0, $count = 0; $i < $city_list && $count < 5 ; $i++) {
-            $timestamp = strtotime($city_data->list[$i]->dt_txt);
-            $forecastDate = date("d/m/y", $timestamp);
-            $formattedTime = date("H:i", $timestamp);
-
-            if ($timestamp >= $currentTimestamp) {
-                echo '<div class="item">';
-                echo '<h1>', $forecastDate, ' ', $formattedTime, '</h1>';
-                $iconName = $city_data->list[$i]->weather[0]->icon;
-                $iconLink = "https://openweathermap.org/img/wn/" . $iconName . "@2x.png";
-
-                echo "<img alt='weather icons' src='$iconLink'>";
-
-                // General information about the weather
-                echo '<h2>Temperature</h2>';
-                echo '<p><strong>Current: </strong>', $city_data->list[$i]->main->temp, '&deg; C</p>';
-                echo '<p><strong>Minimum: </strong>', $city_data->list[$i]->main->temp_min, '&deg; C</p>';
-                echo '<p><strong>Maximum: </strong>', $city_data->list[$i]->main->temp_max, '&deg; C </p>';
-
-                // Something about the air
-                echo '<h2>Air</h2>';
-                echo '<p><strong>Humidity: </strong>', $city_data->list[$i]->main->humidity, '%</p>';
-                echo '<p><strong>Pressure: </strong>', $city_data->list[$i]->main->pressure, 'hpa</p>';
-
-                // Some info about the wind
-                echo '<h2>Wind</h2>';
-                echo '<p><strong>Speed: </strong>', $city_data->list[$i]->wind->speed, 'm/s</p>';
-                echo '<p><strong>Orientation: </strong>', $city_data->list[$i]->wind->deg, '&deg; </p>';
-
-                // Description
-                echo '<h2>The weather</h2>';
-                echo '<p><strong>Description: </strong>', $city_data->list[$i]->weather[0]->description, '</p>';
-                echo '</div>';
-                $count++;
-            }
-        }
-        echo '</div>';
+    if ($response_headers[0] === 'HTTP/1.1 404 Not Found') {
+        echo 'City not found';
     } else {
-        echo '<div class="error">City not found</div>';
+        try {
+            $json_data = file_get_contents($apiUrl);
+
+            if ($json_data === false) {
+                throw new Exception('Failed to retrieve data from the API.');
+            }
+
+            $city_data = json_decode($json_data);
+
+            if ($city_data === null || !isset($city_data->city)) {
+                throw new Exception("City not found");
+            }
+
+            $city_data = json_decode($json_data);
+
+            // Check if the 'city' key exists in the response data to avoid errors
+            if (isset($city_data->city)) {
+                $city_list = count($city_data->list);
+
+                echo '<div class="city_title">';
+                echo '<h1>', $city_data->city->name, '(', $city_data->city->country, ')</h1>';
+                echo '</div>';
+
+                echo '<div class="container">';
+
+                $currentTimestamp = time(); // Aktuelle Serverzeit in Unix-Timestamp erhalten
+                $today = date("d/m/y", $currentTimestamp); // Heutiges Datum
+
+                for ($i = 0, $count = 0; $i < $city_list && $count < 5; $i++) {
+                    $timestamp = strtotime($city_data->list[$i]->dt_txt);
+                    $forecastDate = date("d/m/y", $timestamp);
+                    $formattedTime = date("H:i", $timestamp);
+
+                    if ($timestamp >= $currentTimestamp) {
+                        echo '<div class="item">';
+                        echo '<h1>', $forecastDate, ' ', $formattedTime, '</h1>';
+                        $iconName = $city_data->list[$i]->weather[0]->icon;
+                        $iconLink = "https://openweathermap.org/img/wn/" . $iconName . "@2x.png";
+
+                        echo "<img alt='weather icons' src='$iconLink'>";
+
+                        // General information about the weather
+                        echo '<h2>Temperature</h2>';
+                        echo '<p><strong>Current: </strong>', $city_data->list[$i]->main->temp, '&deg; C</p>';
+                        echo '<p><strong>Minimum: </strong>', $city_data->list[$i]->main->temp_min, '&deg; C</p>';
+                        echo '<p><strong>Maximum: </strong>', $city_data->list[$i]->main->temp_max, '&deg; C </p>';
+
+                        // Something about the air
+                        echo '<h2>Air</h2>';
+                        echo '<p><strong>Humidity: </strong>', $city_data->list[$i]->main->humidity, '%</p>';
+                        echo '<p><strong>Pressure: </strong>', $city_data->list[$i]->main->pressure, 'hpa</p>';
+
+                        // Some info about the wind
+                        echo '<h2>Wind</h2>';
+                        echo '<p><strong>Speed: </strong>', $city_data->list[$i]->wind->speed, 'm/s</p>';
+                        echo '<p><strong>Orientation: </strong>', $city_data->list[$i]->wind->deg, '&deg; </p>';
+
+                        // Description
+                        echo '<h2>The weather</h2>';
+                        echo '<p><strong>Description: </strong>', $city_data->list[$i]->weather[0]->description, '</p>';
+                        echo '</div>';
+                        $count++;
+                    }
+                }
+                echo '</div>';
+            } else {
+                echo '<div class="error">City not found</div>';
+            }
+
+        } catch (Exception $e) {
+            echo 'Message: ' . $e->getMessage();
+        }
+
     }
 }
+
 ?>
 
 </body>
